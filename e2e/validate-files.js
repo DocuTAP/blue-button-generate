ad216ed8e65488b3abddbc646ba29d9bddc539e3
sjakos <sjakos@users.noreply.github.com>
2018-08-20 10:37:39 -0500

    test(error-checking): update validation test to fail on errors
    Jira: XDS-102

--- e2e/validate-files.js
+++ e2e/validate-files.js
@@ -5,8 +5,7 @@ const request = require('request');
   const filePaths = await getFiles();
   const passed = await validateFiles(filePaths);
   console.log(`Success: ${passed}`);
-  // Temporarily disabled
-  // process.exit(passed? 0 : 1);
+  process.exit(passed ? 0 : 1);
 })();
 
 async function validateFiles(filePaths) {
@@ -19,7 +18,8 @@ async function validateFile(filePath) {
   const url =
     'https://ttpds.sitenv.org:8443/referenceccdaservice/?validationObjective=C-CDA_IG_Plus_Vocab&referenceFileName=Readme.txt';
   return new Promise((resolve, reject) => {
-    request.post({
+    request.post(
+      {
         url,
         formData: {
           ccdaFile: fs.createReadStream(filePath)
@@ -50,20 +50,32 @@ async function getFiles() {
 
 function validateResults(results, filePath) {
   const fileName = getFileName(filePath);
-  const errorCount = results.reduce((p, c) => {
-    if (c.type.includes('Error') && c.count !== 0) {
-      // console.log(`Found ${c.count} ${c.type}s in ${fileName}`);
-      p += c.count;
-    }
+  const messageCounts = getReturnedMessageCount(results, fileName);
 
-    return p;
-  }, 0);
-
-  if (errorCount !== 0) {
-    console.log(`Found ${errorCount} total errors in ${fileName}`);
+  if (messageCounts.error !== 0) {
+    console.log(`Found ${messageCounts.error} total errors in ${fileName}`);
   }
 
-  return errorCount === 0;
+  return messageCounts.error === 0;
+}
+
+function getReturnedMessageCount(results, fileName) {
+  return results.reduce((p, c) => {
+    const messageType = getMessageType(c);
+
+    if (c.count !== 0) {
+      console.log(`Found ${c.count} ${c.type} messages in ${fileName}`);
+    }
+
+    p[messageType] = p[messageType] ? p[messageType] + c.count : c.count;
+
+    return p;
+  }, {});
+}
+
+function getMessageType(message) {
+  const messageLabelArray = message.type.split(' ');
+  return messageLabelArray[messageLabelArray.length - 1].toLowerCase();
 }
 
 function getFileName(path) {
